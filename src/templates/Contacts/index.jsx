0c957d32a94b88ffbd837965e3bd66cf45e2a16c
Alim Mambetov <alimbo333@gmail.com>
2024-08-20 14:21:01 +0300

    contacts style

--- src/templates/Contacts/index.jsx
+++ src/templates/Contacts/index.jsx
@@ -7,10 +7,30 @@ import PhoneIcon from '@mui/icons-material/PhoneOutlined';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import { ScrollAnimate } from '@/contexts';
-import { format } from '@/scripts';
+import { format, media } from '@/scripts';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 
 export default ({className}) => {
+	const {phone, telegram, mail} = media;
 	
+	const notify = (message='') => {
+		toast(message, {
+			className: cls.toast,
+			progressClassName: cls.toast__progress,
+			position: "top-right",
+			autoClose: 1000,
+      closeOnClick: true,
+      closeButton: false,
+			theme: "dark",
+			type: 'message'
+		});
+
+	};
+
+
 	return (<>
 		<div id='contacts' container='' className={clx(cls.wrap, className)}>
 			<ScrollAnimate><h2 data-title='Контакты'>Свяжитесь с нами</h2></ScrollAnimate>
@@ -20,24 +40,29 @@ export default ({className}) => {
 				<p>У вас есть вопросы или вам нужна помощь? Свяжитесь с нами, любым удобным для вас способом:</p>
 				<hr />
 				<ul className={cls.list}>
-					<a className={cls.list__item} target='_blank' href='mailto:yasir.academy.8866@gmail.com' >
-						<div data-icon='mail'><MainIcon/></div>
-						<div data-value>yasir.academy.8866@gmail.com</div>
-					</a>
-					<a className={cls.list__item} target='_blank' href='tel:+79640478770' >
-						<div data-icon='phone'><PhoneIcon/></div>
-						<div data-value>{format.phone('+79640478770')}</div>
-					</a>
+					<div className={cls.list__item}  >
+						<a target='_blank' href={`mailto:${mail}`} data-icon='mail'><MainIcon/></a>
+						<CopyToClipboard text={mail}>
+							<button onClick={() => notify('Почта скопирована')} data-value>{mail}</button>
+						</CopyToClipboard>
+					</div>
+					<div className={cls.list__item} >
+						<a target='_blank' href={`tel:${phone}`} data-icon='phone'><PhoneIcon/></a>
+						<CopyToClipboard text={phone}>
+							<button onClick={() => notify('Номер скопирован')} data-value>{format.phone(phone)}</button>
+						</CopyToClipboard>
+					</div>
 				</ul>
 				<ul className={cls.socials}>
-					<a className={cls.socials__item} href='https://t.me/Alim_bo' >
+					<a className={cls.socials__item} href={telegram} >
 						<TelegramIcon/>
 					</a>
-					<a className={cls.socials__item} href='https://wa.me/+79994921293' >
+					<a className={cls.socials__item} href={`https://wa.me/${phone}`} >
 						<WhatsAppIcon/>
 					</a>
 				</ul>
 			</ScrollAnimate>
 		</div>
+		<ToastContainer />
 	</>);
 }
